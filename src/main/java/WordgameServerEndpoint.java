2aede3b6978169c2d8113291aebf2bb1c4111b94
emapiric <57675835+emapiric@users.noreply.github.com>
2020-03-19 23:16:26 +0100

    proba da se konektuje s andoriidom

--- src/main/java/WordgameServerEndpoint.java
+++ src/main/java/WordgameServerEndpoint.java
@@ -10,49 +10,58 @@ public class WordgameServerEndpoint {
     private Logger logger = Logger.getLogger(this.getClass().getName());
 
     @OnOpen
-    public void onOpen(Session session) {
-        logger.info("Connected ... " + session.getId());
+    public void onOpen(Session session) throws IOException {
+        session.getBasicRemote().sendText("Cao");
+        System.out.println("conneetion opened");
     }
 
     @OnMessage
-    public String onMessage(String unscrambledWord, Session session) {
-        switch (unscrambledWord) {
-            case "start":
-                logger.info("Starting the game by sending first word");
-                String scrambledWord = WordRepository.getInstance().getRandomWord().getScrambledWord();
-                session.getUserProperties().put("scrambledWord", scrambledWord);
-                return scrambledWord;
-            case "quit":
-                logger.info("Quitting the game");
-                try {
-                    session.close(new CloseReason(CloseReason.CloseCodes.NORMAL_CLOSURE, "Game finished"));
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-        }
-        String scrambledWord = (String) session.getUserProperties().get("scrambledWord");
-        return checkLastWordAndSendANewWord(scrambledWord, unscrambledWord, session);
+    public synchronized void processMessage(Session session, String
+            message) {
+        System.out.println(message);
     }
+
+//    @OnMessage
+//    public String onMessage(String unscrambledWord, Session session) {
+//        System.out.println(unscrambledWord);
+//        return unscrambledWord;
+////        switch (unscrambledWord) {
+////            case "start":
+////                logger.info("Starting the game by sending first word");
+////                String scrambledWord = WordRepository.getInstance().getRandomWord().getScrambledWord();
+////                session.getUserProperties().put("scrambledWord", scrambledWord);
+////                return scrambledWord;
+////            case "quit":
+////                logger.info("Quitting the game");
+////                try {
+////                    session.close(new CloseReason(CloseReason.CloseCodes.NORMAL_CLOSURE, "Game finished"));
+////                } catch (IOException e) {
+////                    throw new RuntimeException(e);
+////                }
+////        }
+////        String scrambledWord = (String) session.getUserProperties().get("scrambledWord");
+////        return checkLastWordAndSendANewWord(scrambledWord, unscrambledWord, session);
+//    }
 
     @OnClose
     public void onClose(Session session, CloseReason closeReason) {
-        logger.info(String.format("Session %s closed because of %s", session.getId(), closeReason));
+     //   logger.info(String.format("Session %s closed because of %s", session.getId(), closeReason));
     }
 
-    private String checkLastWordAndSendANewWord(String scrambledWord, String unscrambledWord, Session session) {
-        WordRepository repository = WordRepository.getInstance();
-        Word word = repository.getWord(scrambledWord);
-
-        String nextScrambledWord = repository.getRandomWord().getScrambledWord();
-
-        session.getUserProperties().put("scrambledWord", nextScrambledWord);
-
-        String correctUnscrambledWord = word.getUnscrambbledWord();
-
-        if (word == null || !correctUnscrambledWord.equals(unscrambledWord)) {
-            return String.format("You guessed it wrong. Correct answer %s. Try the next one .. %s",
-                    correctUnscrambledWord, nextScrambledWord);
-        }
-        return String.format("You guessed it right. Try the next word ...  %s", nextScrambledWord);
-    }
+//    private String checkLastWordAndSendANewWord(String scrambledWord, String unscrambledWord, Session session) {
+//        WordRepository repository = WordRepository.getInstance();
+//        Word word = repository.getWord(scrambledWord);
+//
+//        String nextScrambledWord = repository.getRandomWord().getScrambledWord();
+//
+//        session.getUserProperties().put("scrambledWord", nextScrambledWord);
+//
+//        String correctUnscrambledWord = word.getUnscrambbledWord();
+//
+//        if (word == null || !correctUnscrambledWord.equals(unscrambledWord)) {
+//            return String.format("You guessed it wrong. Correct answer %s. Try the next one .. %s",
+//                    correctUnscrambledWord, nextScrambledWord);
+//        }
+//        return String.format("You guessed it right. Try the next word ...  %s", nextScrambledWord);
+//    }
 }
